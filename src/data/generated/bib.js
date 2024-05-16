a8ab455e6eaeb03c9b86fb89f9e53364ebefb8a7
Zach Gao <zihegao@outlook.com>
2024-05-16 15:18:08 +0100

    update: new ref

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -13,5 +13,20 @@
         "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
         "volume": "22",
         "year": "2016"
+    },
+    "zhangAddingConditionalControl2023": {
+        "abstract": "We present ControlNet, a neural network architecture to add spatial conditioning controls to large, pretrained text-to-image diffusion models. ControlNet locks the production-ready large diffusion models, and reuses their deep and robust encoding layers pretrained with billions of images as a strong backbone to learn a diverse set of conditional controls. The neural architecture is connected with \"zero convolutions\" (zero-initialized convolution layers) that progressively grow the parameters from zero and ensure that no harmful noise could affect the finetuning. We test various conditioning controls, eg, edges, depth, segmentation, human pose, etc, with Stable Diffusion, using single or multiple conditions, with or without prompts. We show that the training of ControlNets is robust with small ({\\textless}50k) and large ({\\textgreater}1m) datasets. Extensive results show that ControlNet may facilitate wider applications to control image diffusion models.",
+        "author": "Zhang, Lvmin and Rao, Anyi and Agrawala, Maneesh",
+        "doi": "10.48550/arXiv.2302.05543",
+        "file": "arXiv Fulltext PDF:D\\:\\\\Files\\\\Zotero\\\\storage\\\\T7DCRJYY\\\\Zhang et al. - 2023 - Adding Conditional Control to Text-to-Image Diffus.pdf:application/pdf;arXiv.org Snapshot:D\\:\\\\Files\\\\Zotero\\\\storage\\\\CHML7KSA\\\\2302.html:text/html",
+        "keywords": "Computer Science - Artificial Intelligence, Computer Science - Computer Vision and Pattern Recognition, Computer Science - Graphics, Computer Science - Human-Computer Interaction, Computer Science - Multimedia, ControlNet",
+        "month": "nov,",
+        "note": "arXiv:2302.05543 [cs] version: 3",
+        "publisher": "arXiv",
+        "title": "Adding {Conditional} {Control} to {Text}-to-{Image} {Diffusion} {Models}",
+        "type": "misc",
+        "url": "http://arxiv.org/abs/2302.05543",
+        "urldate": "2024-03-28",
+        "year": "2023"
     }
 };
